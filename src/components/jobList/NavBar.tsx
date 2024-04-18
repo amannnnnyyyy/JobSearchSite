6eab07df145083fc258c398d029ba9e7e6f44c16
Amanuel <amanuelmoha.official@gmail.com>
2024-04-18 14:53:19 +0300

    Components for all

--- src/components/jobList/NavBar.tsx
+++ src/components/jobList/NavBar.tsx
@@ -13,7 +13,7 @@ const NavBar = () => {
             <a className="flex flex-shrink-0 items-center mr-4" href="/index.html">
               <img
                 className="h-10 w-auto"
-                src="images/logo.png"
+                src="../../../images/logo.png"
                 alt="React Jobs"
               />
               <span className="hidden md:block text-white text-2xl font-bold ml-2"
