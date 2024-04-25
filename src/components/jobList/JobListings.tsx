3c75ecef76ba746356d2bd30381beda1825dabb5
Amanuel <amanuelmoha.official@gmail.com>
2024-04-25 15:50:22 +0300

    add loading spinner

--- src/components/jobList/JobListings.tsx
+++ src/components/jobList/JobListings.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import jobs from '../../jobs.json'
-import JobListing from './JobListing';
+import React, { useEffect, useState } from 'react'
+import JobListing from './JobListing'
+import Spinner from './spinner'
+
 
 const JobListings = ({isHome = false}) => {
-  const listOfJobs = isHome?jobs.jobs.slice(0,3):jobs.jobs;
-  console.log(jobs);
+  //const listOfJobs = isHome?jobs.jobs.slice(0,3):jobs.jobs;
+  //console.log(jobs);
+
+  const [jobs,setJobs] = useState([]);
+  const [loading,setLoading] = useState(true);
+  useEffect( () =>{
+    const fetchJobs = async()=>{
+      const apiURl = isHome?'/api/jobs?_limit=3':'/api/jobs';
+      try{
+        const response = await fetch(apiURl);
+        const data = await response.json();
+        setJobs(data);
+
+      }catch(error){console.log("Error fetching data : ",error);} finally{setLoading(false);}
+    }
+    fetchJobs();
+  },[]);
+
   return (
     <>
     {/* <!-- Browse Jobs --> */}
@@ -13,11 +30,16 @@ const JobListings = ({isHome = false}) => {
         <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
           {isHome?'Recent Jobs':'Browse Jobs'}
         </h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {listOfJobs.map((job)=>(
+        
+          {loading?(<Spinner loading={loading}/>):(
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            
+          {jobs.map((job)=>(
           <JobListing key={job.id} job={job}/>
         ))}
-        </div>
+            </div>
+          )}
+        
       </div>
     </section>
     </>
